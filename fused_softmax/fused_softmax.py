4e04efd8a706b7bd1a6a1e1a33464fc8c5239615
TheJDen <jadenrodriguez7@gmail.com>
2025-07-20 04:32:40 +0000

    benchmark

--- fused_softmax/fused_softmax.py
+++ fused_softmax/fused_softmax.py
@@ -147,5 +147,36 @@ def test_softmax_kernel(size: tuple, atol=1e-3, rtol=1e-3, device=DEVICE):
     torch.testing.assert_close(z_triton, z_torch, atol=atol, rtol=rtol)
     print("PASSED")
 
+@triton.testing.perf_report(
+    triton.testing.Benchmark(
+        x_names=['N'],
+        x_vals = [128 * i for i in range(2, 100)],
+        line_arg="provider",
+        line_vals=["triton", "torch"],
+        line_names=["Triton", "Torch"],
+        styles=[("blue", '-'), ('green', '-')],
+        ylabel="GB/s",
+        plot_name="softmax-performance",
+        args={'M': 4096}
+
+    )
+)
+def benchmark(M, N, provider):
+    x = torch.randn(M, N, device=DEVICE, dtype=torch.float32)
+
+    stream = getattr(torch, DEVICE.type).Stream()
+    getattr(torch, DEVICE.type).set_stream(stream)
+
+    if provider == "torch":
+        ms = triton.testing.do_bench(lambda: torch.softmax(x, axis=-1))
+    else:
+        ms = triton.testing.do_bench(lambda: softmax(x)) 
+    gbps = lambda ms: 2 * x.numel() * x.element_size() * 1e-9 / (ms * 1e-3)
+    return gbps(ms)
+
 if __name__ == "__main__":
     test_softmax_kernel(size=(1823, 781))
+
+    import sys
+    if len(sys.argv) > 1 and sys.argv[1] == '--benchmark':
+        benchmark.run(save_path='.', print_data=False)
